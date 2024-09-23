6469208661ca3985a252facdcbf69a02c058023a
Swoerve <swoerve@gmail.com>
2024-09-23 15:22:52 +0200

    symetrycal functionality and fixed some variables

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ bot = discord.Bot(intents=intents)
 # Get the webhooks from env
 wb_url_jsu24 = str(os.getenv("WH-JSU24"))
 wb_url_feu24 = str(os.getenv("WH-FEU24"))
+ch_jsu24 = int(os.getenv("CH-JSU24"))
+ch_feu24 = int(os.getenv("CH-FEU24"))
 
 @bot.event
 async def on_ready():
@@ -28,9 +30,9 @@ async def on_message(message: discord.Message):
     async with ClientSession() as session:
         if not message.author.bot:
             
-            chn1 = bot.get_channel(1137065839636533268)
-            chnjsu = bot.get_channel(1286639089583656960)
-            if message.channel.id == 1137065839636533268:
+            chnjsu = bot.get_channel(ch_jsu24)
+            chnfeu = bot.get_channel(ch_feu24)
+            if message.channel.id == ch_feu24:
                 print(message.content + ", " + message.author.name + ", " + str(message.channel.id))
                 #await chnjsu.send(message.author.name + ": " + message.content)
                 webhook = discord.Webhook.from_url(wb_url_jsu24, session=session)
@@ -44,11 +46,20 @@ async def on_message(message: discord.Message):
                 else:
                     await webhook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.display_avatar)
 
-            if message.channel.id == 1286639089583656960:
+            if message.channel.id == ch_jsu24:
                 print(message.content + ", " + message.author.name + ", " + str(message.channel.id))
                 #await chn1.send(message.author.name + ": " + message.content)
                 webhook = discord.Webhook.from_url(wb_url_feu24, session=session)
-                await webhook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.display_avatar)
+                if len(message.attachments) > 0:
+                    newcontent = ""
+                    newcontent += message.content
+                    for i in message.attachments:
+                        newcontent += i.url
+                        newcontent += " "
+                    await webhook.send(content=newcontent, username=message.author.display_name, avatar_url=message.author.display_avatar)
+                else:
+                    await webhook.send(content=message.content, username=message.author.display_name, avatar_url=message.author.display_avatar)
+
 
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
